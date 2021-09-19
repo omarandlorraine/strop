0667374d74b72f65b0e353fe70e124e6a5c28e45
Sam M W <you@example.com>
2021-09-19 15:24:21 +0100

    Rejig parameters

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ use crate::machine::Instruction;
 use crate::machine::State;
 use crate::machine::{set_a, get_a, set_b, get_b, set_x, set_y, get_x, get_y};
 
-use crate::search::SearchData;
+use crate::search::{Schema, SearchData};
 use crate::search::exhaustive_search;
 use crate::search::dead_code_elimination;
 
@@ -42,11 +42,6 @@ const M_OPTS: [MOpt; 10] = [
 	MOpt {name: "pic14",        func: pic14,        help: "PIC14"},
 	MOpt {name: "pic16",        func: pic16,        help: "PIC16"},
 	MOpt {name: "z80",          func: z80,          help: "Zilog Z80"},
-];
-
-const S_OPTS: [SOpt; 2] = [
-	SOpt {name: "exh", func: exhaustive_search,     help: "exhaustive search"},
-	SOpt {name: "dce", func: dead_code_elimination, help: "stochastic dead code elimination"},
 ];
 
 const V_OPTS: [VOpt; 4] = [
@@ -93,20 +88,8 @@ fn mach(m: String) -> Vec<Instruction> {
 	process::exit(1);
 }
 
-fn check_arities(data: &SearchData, m: &String, ins: usize, outs: usize) {
-	if data.live_in.len() != ins {
-		println!("the {} function needs exactly {} live-in(s)", m, ins);
-		process::exit(1);
-	}
-	if data.live_out.len() != outs {
-		println!("the {} function needs exactly {} live-out(s)", m, outs);
-		process::exit(1);
-	}
-}
-
 fn function(m: String, data: &mut SearchData) {
 	if m == "id" {
-		check_arities(data, &m, 1, 1);
 		for n in -128..=127 {
 			data.test_cases.push((vec![n], vec![n]));
 		}
@@ -118,7 +101,6 @@ fn function(m: String, data: &mut SearchData) {
 		let a = arg.parse::<i8>();
 
 		if let Some(f) = a.ok() {
-			check_arities(data, &m, 1, 1);
 			for n in -128_i8..=127 {
 				if let Some(res) = n.checked_mul(f) {
 					data.test_cases.push((vec![n], vec![res]));
@@ -134,17 +116,6 @@ fn function(m: String, data: &mut SearchData) {
 }
 
 fn search(m: String, data: &mut SearchData) {
-	for s_opt in &S_OPTS {
-		if s_opt.name == m {
-			(s_opt.func)(data);
-			return;
-		}
-	}
-	println!("You didn't pick a valid search strategy, so here's the ones I know:");
-	for s_opt in &S_OPTS {
-		println!("\t{}  {}", format!("{:>12}", s_opt.name), s_opt.help);
-	}
-	process::exit(1);
 }
 
 fn parse_live_in<'a>(arg: String) -> Box<dyn for<'r> Fn(&'r mut State, i8) + 'a > {
@@ -176,9 +147,13 @@ fn parse_live_out<'a>(arg: String) -> Box<dyn for<'r> Fn(&'r State) -> Option<i8
 fn main() {
 	let opts: Opts = argh::from_env();
 	let mut data = SearchData::new();
-	data.instrs = mach(opts.arch);
-	data.live_in = opts.live_in.into_iter().map(|arg| parse_live_in(arg)).collect();
-	data.live_out = opts.live_out.into_iter().map(|arg| parse_live_out(arg)).collect();
+	data.instrs = Vec::new();
+	let schema =  Schema::new(
+		opts.live_in.into_iter().map(|arg| parse_live_in(arg)).collect(),
+		opts.live_out.into_iter().map(|arg| parse_live_out(arg)).collect());
 	function(opts.function, &mut data);
+	if opts.search == "exh" {
+		exhaustive_search(&data.test_cases, schema, mach(opts.arch));
+	}
 	search(opts.search, &mut data);
 }
--- src/search.rs
+++ src/search.rs
@@ -23,19 +23,30 @@ impl SearchData<'_> {
 	}
 }
 
-fn run_program(d: &SearchData, inputs: &Vec<i8>) -> Option<State> {
-	let mut s = State::new();
-	
-	for (func, val) in d.live_in.iter().zip(inputs) {
-		(func)(&mut s, *val);
-	}
-	d.prog.iter().fold(Some(s), |mut state, i| (i.operation)(i, &mut state))
+pub struct Schema<'a> {
+	live_in: Vec<Box<dyn for<'r> Fn(&'r mut State, i8)>>,
+	live_out: Vec<Box<dyn for<'r> Fn(&'r State) -> Option<i8> + 'a >>,
 }
 
-fn equivalence(d: &SearchData) -> bool {
-	for tc in &d.test_cases {
-		if let Some(state) = run_program(d, &tc.0) {
-			for (func, val) in d.live_out.iter().zip(&tc.1) {
+impl<'a> Schema<'_> {
+	pub fn new(live_in: Vec<Box<dyn for<'r> Fn(&'r mut State, i8)>>, live_out: Vec<Box<dyn for<'r> Fn(&'r State) -> Option<i8> + 'a >>) -> Schema {
+		Schema { live_in, live_out }
+	}
+}
+
+fn run_program(prog: &Vec<Instruction>, schema: &Schema, inputs: &Vec<i8>) -> Option<State> {
+	let mut s = State::new();
+	
+	for (func, val) in schema.live_in.iter().zip(inputs) {
+		(func)(&mut s, *val);
+	}
+	prog.iter().fold(Some(s), |mut state, i| (i.operation)(i, &mut state))
+}
+
+fn equivalence(prog: &Vec<Instruction>, schema: &Schema, test_cases: &Vec<(Vec<i8>, Vec<i8>)>) -> bool {
+	for tc in test_cases {
+		if let Some(state) = run_program(prog, schema, &tc.0) {
+			for (func, val) in schema.live_out.iter().zip(&tc.1) {
 				let result = func(&state);
 				if result != Some(*val) {
 					return false;
@@ -55,36 +66,36 @@ fn disassemble(p: &Vec<Instruction>) {
 	}
 }
 
-pub fn exhaustive_search(d: &mut SearchData) {
+pub fn exhaustive_search(test_cases: &Vec<(Vec<i8>, Vec<i8>)>, schema: Schema, instrs: Vec<Instruction>) {
 	// There's gotta be a less moronic way of doing this.
 
 	println!("Trying programs of length 1.");
-	for i in &d.instrs {
-		d.prog = vec![*i];
-		if equivalence(d) {
-			disassemble(&d.prog);
+	for i in &instrs {
+		let prog = vec![*i];
+		if equivalence(&prog, &schema, test_cases) {
+			disassemble(&prog);
 			return;
 		};
 	}
 
 	println!("Trying programs of length 2.");
-	for i in &d.instrs {
-		for j in &d.instrs {
-			d.prog = vec![*i, *j];
-			if equivalence(d) {
-				disassemble(&d.prog);
+	for i in &instrs {
+		for j in &instrs {
+			let prog = vec![*i, *j];
+			if equivalence(&prog, &schema, test_cases) {
+				disassemble(&prog);
 				return;
 			};
 		}
 	}
 
 	println!("Trying programs of length 3.");
-	for i in &d.instrs {
-		for j in &d.instrs {
-			for k in &d.instrs {
-				d.prog = vec![*i, *j, *k];
-				if equivalence(d) {
-					disassemble(&d.prog);
+	for i in &instrs {
+		for j in &instrs {
+			for k in &instrs {
+				let prog = vec![*i, *j, *k];
+				if equivalence(&prog, &schema, test_cases) {
+					disassemble(&prog);
 					return;
 				};
 			}
