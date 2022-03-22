0a9b506d9319e2fb1a00304e4257835428a62ec6
Sam M W <you@example.com>
2022-03-22 19:37:27 +0000

    don't panic if the program is empty

--- src/search.rs
+++ src/search.rs
@@ -97,11 +97,11 @@ pub fn difference(prog: &BasicBlock, test_run: &TestRun) -> f64 {
                 Step::Run => {
                     let mut pc: usize = 0;
                     for _i in 0..100 {
-                        let offs = prog.instructions[pc].operate(&mut s);
-                        pc += offs as usize;
                         if pc == prog.instructions.len() {
                             break;
                         }
+                        let offs = prog.instructions[pc].operate(&mut s);
+                        pc += offs as usize;
                     }
                 }
                 Step::Set(datum, val) => match datum.width() {
