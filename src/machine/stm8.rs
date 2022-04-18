47effcc0812bb89d7af86c1e7ddccda2c3f25dd2
Sam M W <you@example.com>
2022-04-18 20:39:05 +0100

    add load & store

--- src/machine/stm8.rs
+++ src/machine/stm8.rs
@@ -211,7 +211,11 @@ fn transfers(_mach: Machine) -> Operation {
             _ => Datum::Register(R::Yh),
         }
     }
-    Operation::Move(rando(), rando())
+    match rand::thread_rng().gen_range(0, 3) {
+        0 => Operation::Move(rando(), rando()), // Register-to-register
+        1 => Operation::Move(Datum::Register(R::A), random_absolute()), // store accumulator
+        _ => Operation::Move(random_stm8_operand(), Datum::Register(R::A)) // load accumulator
+    }
 }
 
 pub fn jumps(_mach: Machine) -> Operation {
