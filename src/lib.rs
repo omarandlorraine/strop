4f293edb50e934697ddb6f82071e2844c861bfd9
Sam M W <you@example.com>
2022-05-10 06:40:37 +0100

    name the python module "stroppy"

--- src/lib.rs
+++ src/lib.rs
@@ -90,7 +90,8 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
 /// import the module.
 #[pymodule]
-fn string_sum(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+#[pyo3(name = "stroppy")]
+fn stroppy(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
