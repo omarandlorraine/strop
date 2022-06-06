5b41f8f389e954cbdb9c8ef9e547148d6bb0722f
Sam M W <you@example.com>
2022-06-06 06:49:02 +0100

    add SubtractWithCarry to fuzz tester.

--- src/machine/mos6502.rs
+++ src/machine/mos6502.rs
@@ -1008,7 +1008,7 @@ pub mod tests {
             let a: u8 = random();
             let b: u8 = random();
             let c: bool = random();
-            let d: bool = random();
+            let d: bool = false;
 
             let msg = format!("For {:#04x} {:?}", opcode, op);
             let regr = format!("run_strop({:?}, {:#04x}, {:#04x}, {}, {})", op, a, b, c, d);
@@ -1064,6 +1064,7 @@ pub mod tests {
     #[test]
     fn fuzzer_call() {
         fuzz_dyadic(AddWithCarry, 0x69);
+        fuzz_dyadic(SubtractWithCarry, 0xe9);
         fuzz_dyadic(And, 0x29);
     }
 }
