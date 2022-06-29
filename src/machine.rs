a6e77e435079502ef959b137281714f1b7863e7b
Sam M W <you@example.com>
2022-06-29 22:13:23 +0100

    more lifetimes

--- src/machine.rs
+++ src/machine.rs
@@ -15,7 +15,9 @@ use rand::random;
 #[derive(Clone, Copy)]
 pub struct Machine {
     pub name: &'static str,
-    random_insn: fn() -> Instruction,
+    random_insn: for<'a> fn(
+        &'a BasicBlock<'_, State, Operand, OUD, IUD>,
+    ) -> Instruction<'a, State, Operand, OUD, IUD>,
     reg_by_name: fn(&str) -> Result<Datum, &'static str>,
 }
 
