06a1739f62755b1d746de749fb00866950c430c4
Sam M W <you@example.com>
2022-07-19 17:57:01 +0100

    implement some eight bit ALU operations

--- src/machine.rs
+++ src/machine.rs
@@ -5,6 +5,13 @@ pub mod mos6502;
 pub mod stm8;
 pub mod x80;
 
+pub trait Strop {
+    fn mutate(&mut self);
+    fn random() -> Self
+    where
+        Self: Sized;
+}
+
 pub trait Instruction: std::fmt::Display + Clone + Sized {
     type State: Default;
     fn randomize(&mut self);
--- src/machine/stm8.rs
+++ src/machine/stm8.rs
@@ -1,5 +1,70 @@
 use crate::machine::Instruction;
+use crate::machine::Strop;
+use rand::random;
 use std::collections::HashMap;
+use strop::randomly;
+
+#[derive(Clone, Copy)]
+enum Operand8 {
+    Imm8(u8),
+    Abs(u16),
+}
+
+impl Strop for u8 {
+    fn random() -> u8 {
+        random()
+    }
+
+    fn mutate(&mut self) {
+        randomly!(
+            /* could try incrementing or decrementing */
+            { *self += 1 }
+            { *self -= 1 }
+
+            /* could try flipping a bit */
+            {
+                let bit_select = 1_u8.rotate_left(rand::thread_rng().gen_range(0..8));
+                *self ^= bit_select;
+            }
+        );
+    }
+}
+
+impl Strop for Operand8 {
+    fn random() -> Operand8 {
+        use Operand8::*;
+        randomly!(
+            {Imm8(random())}
+            {Abs(random())}
+        )
+    }
+    fn mutate(&mut self) {
+        use Operand8::*;
+        let e = match self {
+            Imm8(v) => {
+                let e = v;
+                e.mutate();
+                Imm8(*e)
+            }
+            Abs(addr) => Abs(*addr),
+        };
+        *self = e;
+    }
+}
+
+impl Operand8 {
+    fn get_u8(self, s: &Stm8) -> Option<u8> {
+        use Operand8::*;
+        match self {
+            Imm8(x) => Some(x),
+            Abs(addr) => s.read_mem(Some(addr)),
+        }
+    }
+
+    fn get_i8(self, s: &Stm8) -> Option<i8> {
+        self.get_u8(s).map(|v| i8::from_ne_bytes(v.to_ne_bytes()))
+    }
+}
 
 #[derive(Default)]
 pub struct IndexRegister {
@@ -13,6 +78,21 @@ pub struct Stm8 {
     x: IndexRegister,
     y: IndexRegister,
     m: HashMap<u16, Option<u8>>,
+    carry: Option<bool>,
+    halfcarry: Option<bool>,
+    overflow: Option<bool>,
+    zero: Option<bool>,
+    sign: Option<bool>,
+}
+
+impl Stm8 {
+    fn read_mem(&self, addr: Option<u16>) -> Option<u8> {
+        if let Some(addr) = addr {
+            self.m[&addr]
+        } else {
+            None
+        }
+    }
 }
 
 struct Opcode {
@@ -21,10 +101,80 @@ struct Opcode {
 }
 
 #[derive(Clone, Copy)]
-pub struct Stm8Instruction {
-    randomizer: fn(&mut Self),
-    disassemble: fn(&Self, &mut std::fmt::Formatter<'_>) -> std::fmt::Result,
-    implementation: fn(&Self, &mut Stm8),
+struct Alu8Operation {
+    opcode: &'static str,
+    handler: fn(Option<u8>, &mut Stm8),
+}
+
+const ADC: Alu8Operation = Alu8Operation {
+    opcode: "adc",
+    handler: |val, s| {
+        let m = val.map(|v| i8::from_ne_bytes(v.to_ne_bytes()));
+        let a = s.a.map(|v| i8::from_ne_bytes(v.to_ne_bytes()));
+        let r = a
+            .zip(m)
+            .zip(s.carry)
+            .map(|((a, m), c)| a.wrapping_add(m).wrapping_add(if c { 1 } else { 0 }));
+        let carrytests = a
+            .zip(m)
+            .zip(r)
+            .map(|((a, m), r)| (a & m) | (m & !r) | (!r & a));
+        let overflowtests = a
+            .zip(m)
+            .zip(r)
+            .map(|((a, m), r)| ((a & m) | (m & r) | (r & a)) & -64);
+        let carry = carrytests.map(|t| t.leading_zeros() == 0);
+        let zero = r.map(|r| r == 0);
+        let sign = r.map(|r| r.leading_zeros() == 0);
+        let halfcarry = carrytests.map(|t| t & 0x08 != 0);
+        let overflow = overflowtests.map(|t| t != 0 && t != -64);
+    },
+};
+
+const ADD: Alu8Operation = Alu8Operation {
+    opcode: "add",
+    handler: |val, s| {
+        let m = val.map(|v| i8::from_ne_bytes(v.to_ne_bytes()));
+        let a = s.a.map(|v| i8::from_ne_bytes(v.to_ne_bytes()));
+        let r = a.zip(m).map(|(a, m)| a.wrapping_add(m));
+        let carrytests = a
+            .zip(m)
+            .zip(r)
+            .map(|((a, m), r)| (a & m) | (m & !r) | (!r & a));
+        let overflowtests = a
+            .zip(m)
+            .zip(r)
+            .map(|((a, m), r)| ((a & m) | (m & r) | (r & a)) & -64);
+        s.carry = carrytests.map(|t| t.leading_zeros() == 0);
+        s.zero = r.map(|r| r == 0);
+        s.sign = r.map(|r| r.leading_zeros() == 0);
+        s.halfcarry = carrytests.map(|t| t & 0x08 != 0);
+        s.overflow = overflowtests.map(|t| t != 0 && t != -64);
+    },
+};
+
+const AND: Alu8Operation = Alu8Operation {
+    opcode: "and",
+    handler: |m, s| {
+        let r = s.a.zip(m).map(|(a, m)| a & m);
+        s.zero = r.map(|r| r == 0);
+        s.sign = r.map(|r| r & 0x80 != 0);
+    },
+};
+
+#[derive(Clone, Copy)]
+pub enum Stm8Instruction {
+    Alu8(Alu8Operation, Operand8),
+}
+
+impl std::fmt::Display for Operand8 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        use Operand8::*;
+        match self {
+            Imm8(x) => write!(f, "#${:#04x}", x),
+            Abs(addr) => write!(f, "${:#06x}", addr),
+        }
+    }
 }
 
 impl std::fmt::Display for Stm8Instruction {
