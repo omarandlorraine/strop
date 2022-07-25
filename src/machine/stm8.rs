952f4372931b5f5008fcfc4de5585d09fee3b7e6
Sam M W <you@example.com>
2022-07-25 15:24:46 +0100

    cargo fmt

--- src/machine/stm8.rs
+++ src/machine/stm8.rs
@@ -311,9 +311,9 @@ impl Strop for Stm8Instruction {
     fn random() -> Stm8Instruction {
         use Stm8Instruction::*;
         randomly!(
-            { Alu8(random(), Operand8::random()) }
-            { Alu16(random(), random(), Operand16::random()) }
-            )
+        { Alu8(random(), Operand8::random()) }
+        { Alu16(random(), random(), Operand16::random()) }
+        )
     }
 
     fn mutate(&mut self) {
