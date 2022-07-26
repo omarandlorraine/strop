fe73f73ecb8a879cfb8148dd7633538c57def272
Sam M W <you@example.com>
2022-07-26 06:40:37 +0100

    revise argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-extern crate argh;
-use argh::FromArgs;
+extern crate clap;
+use clap::clap_app;
 
 mod machine;
 mod search;
@@ -7,30 +7,6 @@ use crate::machine::mos6502::Instruction6502;
 use crate::machine::stm8::Stm8Instruction;
 use crate::machine::x80::KR580VM1Instruction;
 use crate::search::BasicBlock;
-
-#[derive(FromArgs, PartialEq, Debug)]
-/// command line arguments
-struct Opts {
-    #[argh(option, short = 'm')]
-    /// the name of the architecture.
-    arch: String,
-
-    #[argh(option, short = 'f')]
-    /// file containing the custom test run
-    file: Option<String>,
-
-    #[argh(option)]
-    /// the function to compute
-    function: Option<String>,
-
-    #[argh(option, long = "in")]
-    /// in variables
-    r#in: Vec<String>,
-
-    #[argh(option)]
-    /// out variables
-    out: Vec<String>,
-}
 
 fn disassemble<I: machine::Instruction + std::fmt::Display>(prog: BasicBlock<I>) {
     for p in prog.instructions {
@@ -44,13 +20,41 @@ fn stocsearch<I: machine::Instruction>() {
 }
 
 fn main() {
-    let opts: Opts = argh::from_env();
+    let matches = clap_app!(strop =>
+        (version: "1.0")
+        (author: "Sam M W <sam.magnus.wilson@gmail.com>")
+        (about: "Stochastically generates machine code")
+        (@subcommand kr580vm1 =>
+         (about: "Generates code for the KR580VM1 or Intel 8080")
+         (@arg only80: --only80 "do not permit KR580VM1 specific instructions (i.e., the generated program will be compatible with the Intel 8080)"))
+        (@subcommand stm8 =>
+         (about: "Generates code for the STM8"))
+        (@subcommand mos6502 =>
+         (about: "Generates code for the MOS 6502")
+         (@arg rorbug: --rorbug "avoid the bug in the ROR instruction of very early chips")
+         (@arg cmos: --cmos "allow CMOS instructions (including phx, stz)")
+         (@arg illegal: --illegal "allow illegal instructions (including lax, dcp, anc)"))
+    ).get_matches();
 
-    if &opts.arch == &"stm8" {
-        stocsearch::<Stm8Instruction>();
-    } else if &opts.arch == &"mos6502" {
-        stocsearch::<Instruction6502>();
-    } else if &opts.arch == &"kr580vm1" {
-        stocsearch::<KR580VM1Instruction>();
+    match matches.subcommand() {
+        ("kr580vm1", sub_matches) => {
+            println!("Calling out to kr580vm1");
+        }
+        ("mos6502", sub_matches) => {
+            if let Some(opts) = sub_matches {
+                let cmos = opts.is_present("cmos");
+                let illegal = opts.is_present("illegal");
+                let rorbug = opts.is_present("rorbug");
+                if cmos && rorbug {
+                    println!("Don't specify --cmos and --rorbug together; there are no chips having both CMOS instructions and the ROR bug.");
+                }
+                if cmos && illegal {
+                    println!("Don't specify --cmos and --illegal together; there are no chips having both CMOS instructions and NMOC illegal instructions.");
+                }
+            }
+        }
+        (_, _) => {
+            panic!();
+        }
     }
 }
