d6bb1f24f6cf67a78c5adf6b20aea492292ba308
Sam M W <you@example.com>
2022-07-28 08:19:19 +0100

    first stab at moving from clap2 to clap3

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 extern crate clap;
-use clap::clap_app;
+use clap::{Arg, ArgAction, Command};
 
 mod machine;
 mod search;
@@ -20,42 +20,44 @@ fn stocsearch<I: machine::Instruction>() {
 }
 
 fn main() {
-    let matches = clap_app!(strop =>
-        (version: "1.0")
-        (author: "Sam M W <sam.magnus.wilson@gmail.com>")
-        (about: "Stochastically generates machine code")
-        (@subcommand kr580vm1 =>
-         (about: "Generates code for the KR580VM1 or Intel 8080")
-         (@arg only80: --only80 "do not permit KR580VM1 specific instructions (i.e., the generated program will be compatible with the Intel 8080)"))
-        (@subcommand stm8 =>
-         (about: "Generates code for the STM8"))
-        (@subcommand mos6502 =>
-         (about: "Generates code for the MOS 6502")
-         (@arg rorbug: --rorbug "avoid the bug in the ROR instruction of very early chips")
-         (@arg cmos: --cmos "allow CMOS instructions (including phx, stz)")
-         (@arg illegal: --illegal "allow illegal instructions (including lax, dcp, anc)"))
-        (@arg ins: -i --in +takes_value ... "where to find inputs to the function")
-        (@arg outs: -o --out +takes_value ... "where to put the function's output")
-    ).get_matches();
+    let matches = Command::new("strop")
+        .version("0.1.0")
+        .author("Sam M W <sam.magnus.wilson@gmail.com>")
+        .about("Stochastically generates machine code")
+        .subcommand(
+            Command::new("kr580vm1").about("Generates code for the KR580VM1 or Intel 8080")
+            .arg(Arg::new("only80").action(ArgAction::SetTrue)).about("do not permit KR580VM1 specific instructions (i.e., the generated program will be compatible with the Intel 8080)"))
+        .subcommand(
+            Command::new("stm8").about("Generates code for the STM8"))
+        .subcommand(
+            Command::new("mos6502").about("Generates code for the MOS 6502")
+            .arg(Arg::new("rorbug").action(ArgAction::SetTrue)).about("avoid the bug in the ROR instruction of very early chips")
+            .arg(Arg::new("cmos").action(ArgAction::SetTrue)).about("allow CMOS instructions (including phx, stz)")
+            .arg(Arg::new("illegal").action(ArgAction::SetTrue)).about("allow illegal instructions (including lax, dcp, anc)")
+        )
+        .arg(Arg::new("in").short('i').help("where to find inputs to the function").action(ArgAction::Append))
+        .arg(Arg::new("out").short('o').help("where to put the function's outputs").action(ArgAction::Append))
+    .get_matches();
 
     match matches.subcommand() {
-        ("kr580vm1", sub_matches) => {
+        Some(("kr580vm1", opts)) => {
             println!("Calling out to kr580vm1");
         }
-        ("mos6502", sub_matches) => {
-            if let Some(opts) = sub_matches {
-                let cmos = opts.is_present("cmos");
-                let illegal = opts.is_present("illegal");
-                let rorbug = opts.is_present("rorbug");
-                if cmos && rorbug {
-                    println!("Don't specify --cmos and --rorbug together; there are no chips having both CMOS instructions and the ROR bug.");
-                }
-                if cmos && illegal {
-                    println!("Don't specify --cmos and --illegal together; there are no chips having both CMOS instructions and NMOC illegal instructions.");
-                }
+        Some(("mos6502", opts)) => {
+            let cmos = opts.is_present("cmos");
+            let illegal = opts.is_present("illegal");
+            let rorbug = opts.is_present("rorbug");
+            if cmos && rorbug {
+                println!("Don't specify --cmos and --rorbug together; there are no chips having both CMOS instructions and the ROR bug.");
+            }
+            if cmos && illegal {
+                println!("Don't specify --cmos and --illegal together; there are no chips having both CMOS instructions and NMOC illegal instructions.");
             }
         }
-        (_, _) => {
+        Some((_, _)) => {
+            panic!();
+        }
+        None => {
             panic!();
         }
     }
