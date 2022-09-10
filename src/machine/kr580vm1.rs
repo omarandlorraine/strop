9ecd1a1546c10456d30968bcb2eddbb5e74be8ce
Sam M W <you@example.com>
2022-09-10 15:36:29 +0100

    kr580vm1: add the instructions LXI, LDAX, STAX

--- src/machine/kr580vm1.rs
+++ src/machine/kr580vm1.rs
@@ -9,7 +9,7 @@ use rand::random;
 
 // some clippy warnings disabled for this module because KR580VM1 support is not there yet.
 
-#[derive(Default, Debug)]
+#[derive(Clone, Copy, Default, Debug)]
 #[allow(dead_code, unused_variables)]
 pub struct RegisterPair {
     low: Option<u8>,
@@ -40,6 +40,31 @@ pub enum R16 {
     SP,
 }
 
+/// This enum is the type whose possible values are BC or DE. It is used by
+/// such instructions as LDAX and STAX.
+#[derive(Clone, Copy, Debug)]
+pub enum RegisterPairBorD {
+    BC,
+    DE,
+}
+
+impl From<RegisterPairBorD> for R16 {
+    fn from(item: RegisterPairBorD) -> Self {
+        match item {
+            RegisterPairBorD::BC => R16::BC,
+            RegisterPairBorD::DE => R16::DE,
+        }
+    }
+}
+
+impl RegisterPair {
+    fn get_u16(self) -> Option<u16> {
+        self.high
+            .zip(self.low)
+            .map(|(h, l)| u16::from_be_bytes([h, l]))
+    }
+}
+
 impl R16 {
     fn pick_one(self) -> R8 {
         use R16::*;
@@ -63,10 +88,73 @@ pub struct KR580VM1 {
     h: RegisterPair,
     h1: RegisterPair,
     sp: Option<u16>,
+    /// MF is the memory bank selection bit
+    mf: bool,
     m: HashMap<u16, Option<u8>>,
     m1: HashMap<u16, Option<u8>>,
     /// True if the program ever uses a KR580VM1 extension (i.e. not Intel 8080 compatible)
     kr580vm1_extension: bool,
+}
+
+impl KR580VM1 {
+    fn set_mf(&mut self, val: bool) {
+        self.kr580vm1_extension = true;
+        self.mf = val;
+    }
+
+    fn write_mem(&mut self, addr: Option<u16>, val: Option<u8>) {
+        if let Some(a) = addr {
+            self.m.insert(a, val);
+        }
+    }
+
+    fn write_mem1(&mut self, addr: Option<u16>, val: Option<u8>) {
+        self.kr580vm1_extension = true;
+        if let Some(a) = addr {
+            self.m1.insert(a, val);
+        }
+    }
+
+    fn get_addr(&mut self, register_pair: R16) -> Option<u16> {
+        match register_pair {
+            R16::BC => self.b.get_u16(),
+            R16::DE => self.d.get_u16(),
+            R16::HL => self.h.get_u16(),
+            R16::H1L1 => {
+                self.kr580vm1_extension = true;
+                self.h1.get_u16()
+            }
+            R16::SP => self.sp,
+        }
+    }
+
+    fn load8(&mut self, reg: R8, val: Option<u8>) {
+        match reg {
+            R8::A => self.a = val,
+            R8::B => self.b.high = val,
+            R8::C => self.b.low = val,
+            R8::D => self.d.high = val,
+            R8::E => self.d.low = val,
+            R8::H => self.h.high = val,
+            R8::L => self.h.low = val,
+            R8::H1 => {
+                self.kr580vm1_extension = true;
+                self.h1.high = val
+            }
+            R8::L1 => {
+                self.kr580vm1_extension = true;
+                self.h1.low = val
+            }
+            R8::M => {
+                let addr = self.get_addr(R16::HL);
+                self.write_mem(addr, val);
+            }
+            R8::M1 => {
+                let addr = self.get_addr(R16::HL);
+                self.write_mem1(addr, val);
+            }
+        }
+    }
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -79,6 +167,14 @@ pub enum KR580VM1Instruction {
 
     /// 6.1.3 Данная команда пересылает два байта непосредственных данных в регистровую память.
     Lxi(R16, u8, u8),
+
+    /// 6.1.4 Данная команда пересылает содержимое ячейки памяти в аккумулятор.
+    /// Адрес ячейки памяти находится в регистровой паре BC или DE.
+    Ldax(RegisterPairBorD),
+
+    /// 6.1.5. Данная команда пересылает содержимое аккумулятора в ячейку памяти.
+    /// Адрес ячейки памяти находится в регистровой паре BC или DE.
+    Stax(RegisterPairBorD),
 }
 
 impl std::fmt::Display for R8 {
@@ -113,6 +209,16 @@ impl std::fmt::Display for R16 {
     }
 }
 
+impl std::fmt::Display for RegisterPairBorD {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        use RegisterPairBorD::*;
+        match self {
+            BC => write!(f, "b"),
+            DE => write!(f, "d"),
+        }
+    }
+}
+
 impl std::fmt::Display for KR580VM1Instruction {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
         use KR580VM1Instruction::*;
@@ -120,6 +226,8 @@ impl std::fmt::Display for KR580VM1Instruction {
             Mov(dst, src) => write!(f, "\tmov {}, {}", dst, src),
             Mvi(dst, src) => write!(f, "\tmvi {}, {}", dst, src),
             Lxi(dst, h, l) => write!(f, "\tlxi {}, {:02x}{:02x}h", dst, h, l),
+            Ldax(rp) => write!(f, "\tldax {}", rp),
+            Stax(rp) => write!(f, "\tstax {}", rp),
         }
     }
 }
@@ -145,6 +253,20 @@ impl Strop for R16 {
     {
         use R16::*;
         randomly!({BC} {DE} {HL} {H1L1} {SP})
+    }
+
+    fn mutate(&mut self) {
+        *self = Self::random();
+    }
+}
+
+impl Strop for RegisterPairBorD {
+    fn random() -> Self
+    where
+        Self: Sized,
+    {
+        use RegisterPairBorD::*;
+        randomly!({BC} {DE})
     }
 
     fn mutate(&mut self) {
@@ -178,6 +300,14 @@ impl Instruction for KR580VM1Instruction {
                 { *self = KR580VM1Instruction::Mvi(dst.pick_one(), random()) }
                 );
             }
+            KR580VM1Instruction::Ldax(rp) => {
+                randomly!(
+                    { rp.mutate() }
+                    {*self = KR580VM1Instruction::Mvi(R8::A, random())}
+                    {*self = KR580VM1Instruction::Mov(R8::A, R8::random())}
+                );
+            }
+            KR580VM1Instruction::Stax(rp) => rp.mutate(),
         }
     }
 
@@ -198,6 +328,9 @@ impl Instruction for KR580VM1Instruction {
         randomly!(
         { Mvi(R8::random(), random()) }
         { Mov(R8::random(), R8::random()) }
+        { Lxi(R16::random(), random(), random()) }
+        { Ldax(RegisterPairBorD::random()) }
+        { Stax(RegisterPairBorD::random()) }
         )
     }
 }
