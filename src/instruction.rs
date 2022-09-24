262cb4567e4e3dafc097fa0f7744233af6f315b8
Sam M W <you@example.com>
2022-09-24 22:28:50 +0100

    module for instruction

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,32 +1,14 @@
 //! This module defines the `Instruction` trait and pulls in the architecture-
-//! specific modules.
+//! specific instruction types.
 
 pub mod mos6502;
-
-/// trait for things which may be mutated. This should include `Instruction`,
-/// `BasicBlock`, etc
-pub trait Strop {
-    /// Does a random mutation to the object
-    fn mutate(&mut self);
-
-    /// creates a new object, having a random value
-    fn random() -> Self
-    where
-        Self: Sized;
-}
 
 /// The `Instruction` trait defines the behaviour of an instruction. It is
 /// used by the BasicBlock type, since a basic block is a sequence of
 /// instructions.
-pub trait Instruction: std::fmt::Display + Clone + Sized {
-    /// The state on which the instruction can operate
-    type State: Default;
-
+pub trait Instruction: Clone + Sized {
     /// Randomly mutates the instruction
     fn randomize(&mut self);
-
-    /// Execute the instruction
-    fn operate(&self, s: &mut Self::State);
 
     /// Length of the instruction, in bytes
     fn length(&self) -> usize;
--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@
 //! embedded, and hobbyist retrocomputing.
 
 #![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
-pub mod machine;
+pub mod instruction;
 pub mod search;
 
 pub use rand;
