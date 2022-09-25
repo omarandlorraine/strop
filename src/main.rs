16713f74ac757ca6e760bd01c422f629f570642f
Sam M W <you@example.com>
2022-09-25 21:10:27 +0100

    simple test in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use strop::snippets::Snippet;
+use strop::instruction::mos6502::Instruction6502;
+
 fn main() {
-    println!("Hello, World!");
+    loop {
+        let sn = Snippet::<Instruction6502>::new();
+        if sn.check_use(Instruction6502::sets_x, Instruction6502::reads_x) {
+            sn.disassemble();
+            break;
+        }
+    }
 }
