5a427aa2e3b00f36c085a2503fe20b5fa7edc5f4
Sam M W <you@example.com>
2022-10-02 23:09:22 +0100

    remove unused import

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use strop::mos6502::Instruction6502;
 use strop::snippets::Snippet;
-use strop::z80::InstructionZ80;
 use strop::static_analysis::check_use;
 use strop::mos6502::static_analysis::*;
 
