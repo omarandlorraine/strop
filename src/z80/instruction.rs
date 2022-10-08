4a36f54e05a4369ddd662f010a665c90f5a30b28
Sam M W <you@example.com>
2022-10-08 05:51:10 +0100

    z80 backend: add compatibility with 8080 and SM83

--- src/z80/instruction.rs
+++ src/z80/instruction.rs
@@ -26,6 +26,83 @@ impl InstructionZ80 {
     fn decode(&self) -> DeZ80Instruction {
         DeZ80Instruction::decode_one(&mut self.encoding.as_slice()).unwrap()
     }
+
+    fn compatible_with_sm83(&self) -> bool {
+        //! Returns true if the instruction can be expected to run on the gameboy
+        if !self.compatible_with_8080() {
+            return false;
+        }
+
+        let bytes = self.to_bytes();
+        let (opcode, prefixed) = (bytes[0], bytes[1]);
+
+        if opcode == 0xcb {
+            // On the Z80 this is the (illegal) SL1 instruction, but on the SM83 it's some kind of
+            // SWAP instruction
+            return prefixed & 0xf0 != 0x30;
+        }
+
+        return match opcode {
+            //                     SM83 instruction   Z80 instruction
+            0xf2 => false, //      ld   a,(c)         jp  p,nn
+            0xe2 => false, //      ld   (c),a         jp  nv,nn
+            0xea => false, //      ld   (nn),a        jp  v,nn
+            0xfa => false, //      ld   a,(nn)        jp  m,nn
+            0x3a => false, //      ldd  a,(hl)        ld  a,(nn)
+            0x32 => false, //      ldd  (hl),a        ld  (nn),a
+            0x2a => false, //      ldi  a,(hl)        ld  hl,(nn)
+            0x22 => false, //      ldi  (hl),a        ld  (nn),hL
+            0x08 => false, //      ld   (nn),sp       ex  af,af'
+            0xe0 => false, //      ldh  (n),a         ret nv
+            0xf0 => false, //      ldh  a,(n)         ret P
+            0xf8 => false, //      ld   hl,(sp+e)     ret m
+            0xe8 => false, //      add  sp,e          ret v
+            0x10 => false, //      stop               djnz
+            0xd9 => false, //      reti               exx
+            _ => true,
+        };
+    }
+
+    fn compatible_with_8080(&self) -> bool {
+        //! Returns true if the instruction can be expected to run on the 8080
+        //! (This includes instructions like `ADD`, which has a different effect on the Parity flag
+        //! on the Z80 and on the 8080, so take care.)
+        let opcode = self.to_bytes()[0];
+
+        if opcode & 0x1c != 0x00 {
+            // these opcodes are aliased to NOP on the 8080, but encode various new instructions on
+            // the Z80. EX AF, AF', DJNZ, relative and conditional JR.
+            return false;
+        }
+
+        if opcode == 0xcb {
+            // this opcode is an instruction prefix on the Z80 but aliased to absolute JMP on the
+            // 8080
+            return false;
+        }
+
+        if opcode == 0xd9 {
+            // EXX on the Z80
+            return false;
+        }
+
+        if opcode == 0xdd {
+            // IX prefix on the Z80
+            return false;
+        }
+
+        if opcode == 0xed {
+            // ED prefix for miscellaneous Z80 instructions
+            return false;
+        }
+
+        if opcode == 0xfd {
+            // ED prefix for miscellaneous Z80 instructions
+            return false;
+        }
+
+        return true;
+    }
 }
 
 impl Instruction for InstructionZ80 {
