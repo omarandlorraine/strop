e1936cf2d44443bfcdc20a42ae7d2502d6fc61ea
Sam M W <you@example.com>
2022-10-13 07:01:36 +0100

    try and the iterator style

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use strop::mos6502::static_analysis::*;
 use strop::mos6502::Instruction6502;
 use strop::snippets::Snippet;
 use strop::static_analysis::check_use;
+use strop::generate::Random;
 
 fn main() {
+    /*
     loop {
         let sn = Snippet::<Instruction6502>::new();
         if !check_use(&sn, check_use_c) {
@@ -11,4 +13,22 @@ fn main() {
             break;
         }
     }
+    */
+
+    // generate an initial population
+    let population = Random::<Instruction6502>::new(0x200, 20)
+        // Remove all flow control instructions; we'll end up with a linear program without loops,
+        // subroutine calls, jumps, returns, etc.
+        .map(|p| p.make_bb())
+
+        // by static analysis, remove programs which use a register or flag without initializing it
+        // first.
+        .filter(|p| check_use(p, check_use_c))
+        .filter(|p| check_use(p, check_use_x))
+        .filter(|p| check_use(p, check_use_y))
+
+        // start an initial population of a particular size
+        .take(1000);
+
+    
 }
