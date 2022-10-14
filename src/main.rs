b8e62af556528f271c54e88835083e39db29c756
Sam M W <you@example.com>
2022-10-14 04:54:56 +0100

    main example uses new-style generators now

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,33 @@
+use strop::generate::{McmcSynth, Random};
 use strop::mos6502::static_analysis::*;
 use strop::mos6502::Instruction6502;
 use strop::snippets::Snippet;
 use strop::static_analysis::check_use;
-use strop::generate::Random;
 
 fn main() {
-    /*
-    loop {
-        let sn = Snippet::<Instruction6502>::new();
-        if !check_use(&sn, check_use_c) {
-            sn.disassemble();
-            break;
-        }
-    }
-    */
-
     // generate an initial population
-    let population = Random::<Instruction6502>::new(0x200, 20)
+    let population = Random::<Instruction6502>::new(0x200, 20, mult)
         // Remove all flow control instructions; we'll end up with a linear program without loops,
         // subroutine calls, jumps, returns, etc.
-        .map(|p| p.make_bb())
-
+        .map(|(score, sn)| (score, sn.make_bb()))
         // by static analysis, remove programs which use a register or flag without initializing it
         // first.
-        .filter(|p| check_use(p, check_use_c))
-        .filter(|p| check_use(p, check_use_x))
-        .filter(|p| check_use(p, check_use_y))
-
+        .filter(|(score, sn)| check_use(sn, check_use_c))
+        .filter(|(score, sn)| check_use(sn, check_use_x))
+        .filter(|(score, sn)| check_use(sn, check_use_y))
         // start an initial population of a particular size
         .take(1000);
 
-    
+    // loop until we find at least one candidate program that at least computes the right result
+    while population.any(|(score, sn)| score == 0.0) {
+        let next_generation = population
+            .flat_map(|p| McmcSynth::new(&p.1, mult).take(50))
+            .collect();
+
+        next_generation.sort_by(|a, b| a.0.partial_cmp(&b.0).expect("Tried to compare a NaN"));
+
+        next_generation.truncate(100);
+
+        population = next_generation;
+    }
 }
