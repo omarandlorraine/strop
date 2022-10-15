f89eac90736d1b05932dd06d4fd6a81bf9580e58
Sam M W <you@example.com>
2022-10-15 06:23:47 +0100

    fix some mistakes, it builds again

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,13 @@ use strop::mos6502::Instruction6502;
 use strop::snippets::Snippet;
 use strop::static_analysis::check_use;
 
+fn mult(sn: &Snippet<Instruction6502>) -> f64 {
+    1.0
+}
+
 fn main() {
     // generate an initial population
-    let population = Random::<Instruction6502>::new(0x200, 20, mult)
+    let mut population: Vec<_> = Random::<Instruction6502>::new(0x200, 20, mult)
         // Remove all flow control instructions; we'll end up with a linear program without loops,
         // subroutine calls, jumps, returns, etc.
         .map(|(score, sn)| (score, sn.make_bb()))
@@ -16,11 +20,13 @@ fn main() {
         .filter(|(score, sn)| check_use(sn, check_use_x))
         .filter(|(score, sn)| check_use(sn, check_use_y))
         // start an initial population of a particular size
-        .take(1000);
+        .take(1000)
+        .collect();
 
     // loop until we find at least one candidate program that at least computes the right result
-    while population.any(|(score, sn)| score == 0.0) {
-        let next_generation = population
+    while population.iter().any(|(score, sn)| *score == 0.0) {
+        let mut next_generation: Vec<_> = population
+            .iter()
             .flat_map(|p| McmcSynth::new(&p.1, mult).take(50))
             .collect();
 
