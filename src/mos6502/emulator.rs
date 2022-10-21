62c0fc1676b4d1f0060d8f64b5803d0ab9e91b65
Sam M W <you@example.com>
2022-10-21 09:53:16 +0100

    a few convenience functions for the emulator

--- src/mos6502/emulator.rs
+++ src/mos6502/emulator.rs
@@ -6,6 +6,22 @@ pub struct Emulator6502 {
     cpu: CPU,
 }
 
+impl Emulator6502 {
+    pub fn get_a(&mut self) -> u8 {
+        u8::from_ne_bytes(self.cpu.registers.accumulator.to_ne_bytes())
+    }
+
+    pub fn set_a(&mut self, val: u8) {
+        self.cpu.registers.accumulator = i8::from_ne_bytes(val.to_ne_bytes());
+    }
+}
+
+impl Default for Emulator6502 {
+    fn default() -> Self {
+        Self { cpu: CPU::new() }
+    }
+}
+
 impl Emulator for Emulator6502 {
     fn run(&mut self, org: usize, _budget: u32, bytes: &mut dyn Iterator<Item = u8>) {
         // the emulator uses 0xff as a sentinel to end the currently running program.
