5f5719bf8111a058d13eb368a1eb8a8ed2249668
Sam M W <you@example.com>
2022-10-22 07:07:42 +0100

    simplify the example

--- src/main.rs
+++ src/main.rs
@@ -10,46 +10,33 @@ use strop::static_analysis::check_use;
 fn mult(sn: &Snippet<Instruction6502>) -> f64 {
     let mut emu: Emulator6502 = Default::default();
 
-    emu.run(0x200, 3000, &mut sn.to_bytes().into_iter());
-
-    println!();
-    println!();
-    println!();
+    println!("About to run this");
     sn.disassemble();
+
+    emu.run(0x200, 3000, &mut sn.to_bytes().into_iter());
 
     (emu.get_a().wrapping_sub(45)).into()
 }
 
 fn main() {
-    // generate an initial population
-    let mut population: Vec<_> = Random::<Instruction6502>::new(0x200, 20, mult)
-        // Remove all flow control instructions; we'll end up with a linear program without loops,
-        // subroutine calls, jumps, returns, etc.
-        .map(|(score, sn)| (score, sn.make_bb()))
-        // by static analysis, remove programs which use a register or flag without initializing it
-        // first.
-        .filter(|(_score, sn)| check_use(sn, check_use_c))
-        .filter(|(_score, sn)| check_use(sn, check_use_x))
-        .filter(|(_score, sn)| check_use(sn, check_use_y))
-        // start an initial population of a particular size
-        .take(1000)
-        .collect();
+
+    let parent = Snippet::<Instruction6502>::default();
+    let mut mc = McmcSynth::new(&parent, Constraints::<Instruction6502>::default(), mult);
 
     // loop until we find at least one candidate program that at least computes the right result
-    while population.iter().any(|(score, _sn)| *score == 0.0) {
-        let mut next_generation: Vec<_> = population
-            .iter()
-            .take(100)
-            .flat_map(|p| {
-                McmcSynth::new(&p.1, Constraints::<Instruction6502>::default(), mult).take(50)
-            })
-            .map(|(score, p)| (score, p.make_bb()))
-            .collect();
+    loop {
+        let (score, sn) = mc.next().unwrap();
 
-        next_generation.sort_by(|a, b| a.0.partial_cmp(&b.0).expect("Tried to compare a NaN"));
+        println!("one loop iteration, score {}", score);
+        sn.disassemble();
 
-        next_generation.truncate(100);
+        println!();
+        println!();
 
-        population = next_generation;
+        if score == 0.0 {
+            break;
+        }
     }
+    println!("afterloop");
+
 }
