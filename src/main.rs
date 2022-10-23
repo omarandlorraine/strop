e9890abacb0c09d6966058ff6100eb5d300ec694
Sam M W <you@example.com>
2022-10-23 20:55:25 +0100

    add a multiply example

--- src/main.rs
+++ src/main.rs
@@ -27,10 +27,36 @@ fn set45(sn: &Snippet<Instruction6502>) -> f64 {
     distance
 }
 
+fn mul10(sn: &Snippet<Instruction6502>) -> f64 {
+    // Checks if the snippet loads the number 45 into the accumulator
+    let mut emu: Emulator6502 = Default::default();
+    let mut distance: f64 = 0.0;
+
+    // Set the accumulator to a random number, and if the number times 10 still fits in a byte,
+    // check that the program multiplies the number correctly.
+    let mut count = 50;
+    loop {
+        let before: u8 = random();
+        emu.set_a(before);
+
+        if let Some(result) = before.checked_mul(10) {
+            emu.run(0x200, 3000, &mut sn.to_bytes().into_iter());
+
+            distance += f64::from(emu.get_a().wrapping_sub(result));
+
+            count -= 1;
+            if count == 0 {
+                break;
+            }
+        }
+    }
+    distance
+}
+
 fn main() {
     let constraint = Constraints::<Instruction6502>::new(vec![Instruction6502::perm_bb]);
     let _parent = Snippet::<Instruction6502>::default();
-    let mut cp = CorrectPrograms::new(constraint, set45);
+    let mut cp = CorrectPrograms::new(constraint, mul10);
 
     let prog = cp.next().unwrap().1;
     prog.disassemble();
