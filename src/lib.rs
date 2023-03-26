44ab838e6e3ac14f98b778833a6f6920f5527f76
Sam M W <you@example.com>
2023-03-26 14:49:16 +0100

    move //! comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+//! Superoptimizer written in Rust
+//! ------------------------------
+//! This program stochastically generates assembly language programs that compute a given function.
+//! The idea is you give it a function to compute and specify which registers and things to use,
+//! and strop will generate and output a pretty good program which does the specified thing.
+//!
+//! So far, strop has had a focus on supporting architectures that are not well supported by
+//! mainstream compilers such as LLVM. These have included architectures common in low-end
+//! embedded, and hobbyist retrocomputing.
+
 pub use rand;
 mod backends;
 
@@ -14,16 +24,6 @@ trait Instruction {
     fn length(&self) -> usize;
     fn disassemble(&self) -> String;
 }
-
-//! Superoptimizer written in Rust
-//! ------------------------------
-//! This program stochastically generates assembly language programs that compute a given function.
-//! The idea is you give it a function to compute and specify which registers and things to use,
-//! and strop will generate and output a pretty good program which does the specified thing.
-//!
-//! So far, strop has had a focus on supporting architectures that are not well supported by
-//! mainstream compilers such as LLVM. These have included architectures common in low-end
-//! embedded, and hobbyist retrocomputing.
 
 // The reason I can't pull in randomly! as a dependency is that crates.io seems to require all my
 // dependencies to also be on crates.io.
