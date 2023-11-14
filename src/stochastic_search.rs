8ff94bab18e00d98ddd4d21620a87e9edba02e23
Sam M W <you@example.com>
2023-11-14 12:52:53 +0000

    implement a stochastic search properly

--- src/stochastic_search.rs
+++ src/stochastic_search.rs
@@ -8,14 +8,16 @@ use crate::{Candidate, Instruction, InstructionSet, Lcg};
 pub struct StochasticSearch<I: InstructionSet> {
     instruction_set: I,
     /// The program under current consideration.
-    pub instructions: Candidate<I::Instruction>,
-    fitness: Option<f32>,
+    parent: Candidate<I::Instruction>,
+    child: Candidate<I::Instruction>,
+    parent_score: f32,
+    child_score: f32,
     prng: Lcg,
 }
 
 impl<I: InstructionSet> SearchFeedback for StochasticSearch<I> {
     fn score(&mut self, score: f32) {
-        self.fitness = Some(score);
+        self.child_score = score.abs();
     }
 }
 
@@ -23,73 +25,70 @@ impl<I: InstructionSet> StochasticSearch<I> {
     /// returns a new `Candidate`
     pub fn new(instruction_set: I) -> Self {
         // Empty list of instructions
-        let instructions = Candidate::<I::Instruction>::empty();
-
-        // we don't have a valid fitness score yet
-        let fitness = None;
+        let parent = Candidate::<I::Instruction>::empty();
+        let child = Candidate::<I::Instruction>::empty();
+        let parent_score = f32::MAX;
+        let child_score = f32::MAX;
 
         let prng = Lcg::new(rand::random());
 
-        Self {
-            instruction_set,
-            instructions,
-            fitness,
+        Self { parent, parent_score, child, child_score, instruction_set,
             prng,
         }
     }
 
     fn random_offset(&mut self) -> usize {
         let offset: usize = self.prng.next().unwrap().into();
-        offset % self.instructions.instructions.len()
+        offset % self.child.instructions.len()
     }
 
     fn delete(&mut self) {
         // If the list of instructions contains at least one instruction, then delete one at
         // random.
-        if !self.instructions.instructions.is_empty() {
+        if !self.child.instructions.is_empty() {
             let offset = self.random_offset();
-            self.instructions.instructions.remove(offset);
+            self.child.instructions.remove(offset);
         }
     }
 
     fn insert(&mut self) {
         // Insert a randomly generated instruction at a random location in the program.
-        let offset = if self.instructions.instructions.is_empty() {
+        let offset = if self.child.instructions.is_empty() {
             0
         } else {
             self.random_offset()
         };
-        self.instructions.instructions.insert(offset, I::random());
+        self.child.instructions.insert(offset, I::random());
     }
 
     fn swap(&mut self) {
         // If the program contains at least two instructions, then pick two at random and swap them
         // over.
-        if self.instructions.instructions.len() > 2 {
+        if self.child.instructions.len() > 2 {
             let offset_a = self.random_offset();
             let offset_b = self.random_offset();
-            let instruction_a = self.instructions.instructions[offset_a];
-            let instruction_b = self.instructions.instructions[offset_b];
-            self.instructions.instructions[offset_a] = instruction_b;
-            self.instructions.instructions[offset_b] = instruction_a;
+            let instruction_a = self.child.instructions[offset_a];
+            let instruction_b = self.child.instructions[offset_b];
+            self.child.instructions[offset_a] = instruction_b;
+            self.child.instructions[offset_b] = instruction_a;
         }
     }
 
     fn replace(&mut self) {
         // If the list of instructions contains at least one instruction, then pick one at random
         // and swap it for something totally different.
-        if !self.instructions.instructions.is_empty() {
+        if !self.child.instructions.is_empty() {
             let offset = self.random_offset();
-            self.instructions.instructions[offset] = I::random();
+            self.child.instructions[offset] = I::random();
         }
     }
 
     fn mutate(&mut self) {
         // If the list of instructions contains at least one instruction, then pick one at random
         // and call its `mutate` method.
-        if !self.instructions.instructions.is_empty() {
+        if !self.child.instructions.is_empty() {
             let offset = self.random_offset();
-            self.instructions.instructions[offset].mutate();
+            self.child.instructions[offset].mutate();
         }
     }
 
@@ -112,6 +111,23 @@ impl<I: InstructionSet> Iterator for StochasticSearch<I> {
     type Item = Candidate<<I as InstructionSet>::Instruction>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        None
+        use rand::Rng;
+
+        if self.child_score < self.parent_score {
+            // The child is better than the parent, so definitely remember that
+            self.parent_score = self.child_score.clone();
+            self.parent = self.child.clone();
+        } else if self.child_score > self.parent_score {
+            // The child is worse that the parent. 
+            let num = rand::thread_rng().gen_range(0.0..self.child_score);
+
+            if num > self.parent_score {
+                // Maybe the search has wandered off into the weeds. Try going back to the parent
+                self.child = self.parent.clone();
+                self.child_score = self.parent_score;
+            }
+        }
+        self.random_mutation();
+        Some(self.child.clone())
     }
 }
