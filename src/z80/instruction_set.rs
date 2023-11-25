f0d163d13266f0191f19609baba7968a811f6464
Sam M W <you@example.com>
2023-11-25 04:06:48 +0000

    implement Z80Instruction

---
+++ src/z80/instruction_set.rs
@@ -0,0 +1,193 @@
+//! Module containing definitions for Z80 and 8080 instruction sets
+
+use crate::Instruction;
+use crate::InstructionSet;
+
+/// Represents a Z80 instruction
+#[derive(Clone, Copy)]
+pub struct Z80Instruction {
+    mc: [u8; 5],
+}
+
+impl std::fmt::Debug for Z80Instruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(
+            f,
+            "{:#04x} {:#04x} {:#04x} {:#04x} {:#04x}",
+            self.mc[0], self.mc[1], self.mc[2], self.mc[3], self.mc[4]
+        )
+    }
+}
+
+impl Instruction for Z80Instruction {
+    fn random() -> Self {
+        use rand::random;
+        Self { mc: random() }
+    }
+
+    fn mutate(self) -> Self {
+        use rand::random;
+        use rand::Rng;
+
+        let mut copy = self;
+
+        let offset = rand::thread_rng().gen_range(0..5);
+        if random() {
+            // try flipping a bit at random
+            let bit = rand::thread_rng().gen_range(0..8);
+            copy.mc[offset] ^= 1 << bit;
+        } else {
+            // try straight-up replacing the byte for another
+            copy.mc[offset] = random();
+        }
+
+        copy
+    }
+
+    fn encode(self) -> Vec<u8> {
+        let encoding = Vec::<_>::from(self.mc);
+        // We now have the bytes in the right order, but there's possibly garbage bytes at the end
+        // of the Vec<u8>, because there's variable-length instruction stored in a [u8; 5]. So
+        // therefore I'm going to use the dez80 crate to decode the instruction, and then re-encode
+        // it. The re-encoding of course will not include these garbage bytes.
+
+        let instruction = dez80::Instruction::decode_one(&mut encoding.as_slice()).unwrap();
+        instruction.to_bytes()
+    }
+
+    fn first() -> Self {
+        Self {
+            mc: [0, 0, 0, 0, 0],
+        }
+    }
+
+    fn increment(&mut self) -> Option<Self> {
+        // The length of the instruction
+        let offset = self.encode().len();
+
+        for i in offset + 1..5 {
+            self.mc[i] = 0;
+        }
+
+        fn incr_operand(insn: &mut Z80Instruction, offset: usize) -> bool {
+            if let Some(n) = insn.mc[offset].checked_add(1) {
+                insn.mc[offset] = n;
+                true
+            } else {
+                insn.mc[offset] = 0;
+                if offset > 0 {
+                    incr_operand(insn, offset - 1)
+                } else {
+                    false
+                }
+            }
+        }
+
+        // this prefix is ignored if the following byte is in the set below. In other words, there
+        // are no valid encodings starting with 0xdd or 0xfd and one of the following bytes.
+        // So let's skip it.
+        while matches!(self.mc[0], 0xdd | 0xfd)
+            && matches!(
+                self.mc[1],
+                0x00..=0x18
+                    | 0x1a..=0x20
+                    | 0x27..=0x28
+                    | 0x2f..=0x33
+                    | 0x37..=0x38
+                    | 0x3a..=0x43
+                    | 0x47..=0x4b
+                    | 0x4f..=0x53
+                    | 0x57..=0x5b
+                    | 0x5f
+                    | 0x70
+                    | 0x76
+                    | 0x78..=0x7b
+                    | 0x7f..=0x83
+                    | 0x87..=0x8b
+                    | 0x8f..=0x93
+                    | 0x97..=0x9b
+                    | 0x9f..=0xa3
+                    | 0xa7..=0xab
+                    | 0xaf..=0xb3
+                    | 0xb7..=0xbb
+                    | 0xbf..=0xca
+                    | 0xcc..=0xe0
+                    | 0xe2
+                    | 0xe4
+                    | 0xe6..=0xe8
+                    | 0xea..=0xf8
+                    | 0xfa..=0xff
+            )
+        {
+            incr_operand(self, 1);
+        }
+
+        #[cfg(test)]
+        {
+            let instruction =
+                dez80::Instruction::decode_one(&mut self.encode().as_slice()).unwrap();
+            assert!(instruction.ignored_prefixes.is_empty(), "{:?}", self);
+        }
+
+        // managed to increment the operand
+        if incr_operand(self, offset - 1) {
+            Some(*self)
+        } else {
+            None
+        }
+    }
+}
+
+impl std::fmt::Display for Z80Instruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        let instruction = dez80::Instruction::decode_one(&mut self.encode().as_slice()).unwrap();
+        write!(f, "{}", instruction)
+    }
+}
+
+/// The Z80 instruction set.
+#[derive(Clone, Copy, Debug, Default)]
+pub struct Z80InstructionSet {}
+
+impl InstructionSet for Z80InstructionSet {
+    type Instruction = Z80Instruction;
+
+    fn random(&self) -> Self::Instruction {
+        todo!()
+    }
+
+    fn next(&self, instruction: &mut Self::Instruction) -> Option<()> {
+        instruction.increment()?;
+        Some(())
+    }
+
+    fn mutate(&self, _instruction: &mut Self::Instruction) {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    #[test]
+    fn disassembly() {
+        use crate::InstructionSet;
+        for p in crate::z80::z80().bruteforce_with_maximum_length(1) {
+            p.disassemble();
+        }
+    }
+
+    #[test]
+    fn lengths() {
+        use crate::Instruction;
+
+        use super::Z80Instruction;
+        let mut insn = Z80Instruction::first();
+
+        loop {
+            if insn.increment().is_none() {
+                break;
+            }
+        }
+    }
+}
--- src/z80/mod.rs
+++ src/z80/mod.rs
@@ -1,3 +1,9 @@
 //! The Z80 backend (can of course also be used to generate code for the Intel 8080 or the SM83).
 
 pub mod emulators;
+pub mod instruction_set;
+
+/// Returns the default Z80 instruction set
+pub fn z80() -> instruction_set::Z80InstructionSet {
+    instruction_set::Z80InstructionSet::default()
+}
