7b61d1c626e853d9c588def40e6d8837feae659b
Sam M W <you@example.com>
2024-03-02 08:31:29 +0000

    the search source code has been moved for some reason

---
+++ src/search/mod.rs
@@ -0,0 +1,9 @@
+mod stochastic;
+mod bruteforce;
+
+pub use stochastic::StochasticSearch;
+pub use bruteforce::BruteForceSearch;
+pub use bruteforce::LengthLimitedSearch;
+pub use bruteforce::CompatibilitySearch;
+pub use bruteforce::LinkageSearch;
+pub use bruteforce::SearchTrace;
