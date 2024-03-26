39d1f4dc7d57757f3573d052e00bac27b7420709
Sam M W <you@example.com>
2024-03-26 05:52:50 +0000

    arpa_inet_h now takes advantage of the API's generics

--- examples/arpa_inet_h.rs
+++ examples/arpa_inet_h.rs
@@ -16,24 +16,7 @@ fn ntohs(val: u16) -> Option<u16> {
     Some(u16::from_be_bytes(val.to_le_bytes()))
 }
 
-fn bruteforce32(label: &'static str, func: fn(u32) -> Option<u32>) {
-    use strop::z80::instruction_set::Z80Instruction;
-    use strop::z80::Z80Search;
-    use strop::z80::ZilogZ80;
-    use strop::SearchAlgorithm;
-    use strop::Stochastic;
-
-    println!("{}:", label);
-    Z80Instruction::stochastic_search()
-        .compatibility(ZilogZ80)
-        .z88dkfastcall(func)
-        .iter()
-        .next()
-        .unwrap()
-        .disassemble();
-}
-
-fn bruteforce16(label: &'static str, func: fn(u16) -> Option<u16>) {
+fn bruteforce<T: strop::Scalar>(label: &'static str, func: fn(T) -> Option<T>) {
     use strop::z80::instruction_set::Z80Instruction;
     use strop::z80::Z80Search;
     use strop::z80::ZilogZ80;
@@ -51,8 +34,8 @@ fn bruteforce16(label: &'static str, func: fn(u16) -> Option<u16>) {
 }
 
 fn main() {
-    bruteforce16("htons", htons);
-    bruteforce32("htonl", htonl);
-    bruteforce32("ntohl", ntohl);
-    bruteforce16("ntohs", ntohs);
+    bruteforce("htons", htons);
+    bruteforce("htonl", htonl);
+    bruteforce("ntohl", ntohl);
+    bruteforce("ntohs", ntohs);
 }
