796c7ed634aeb985c9146d0976d879b47e7a52cf
Sam M W <you@example.com>
2024-05-19 21:59:39 +0100

    subroutine

--- src/search/bruteforce.rs
+++ src/search/bruteforce.rs
@@ -7,7 +7,7 @@ use crate::{Candidate, Instruction};
 /// Iterates across the entire search space, shortest programs first.
 #[derive(Debug)]
 pub struct BruteForceSearch<I: Instruction + PartialOrd + PartialEq> {
-    curr: Vec<I>,
+    curr: Candidate<I>,
     max_length: usize,
 }
 
@@ -15,15 +15,15 @@ impl<I: Instruction + std::cmp::PartialOrd> SearchAlgorithm for BruteForceSearch
     type Item = I;
     fn score(&mut self, _: f32) {}
 
-    fn replace<F: Fixup<I>>(&mut self, offset: usize, fixup: F)  -> bool {
-        let orig = self.curr[offset];
+    fn replace<F: Fixup<I>>(&mut self, offset: usize, fixup: F) -> bool {
+        let orig = self.curr.instructions[offset];
         if fixup.check(orig) {
-            if let Some(next) = fixup.next(orig){
+            if let Some(next) = fixup.next(orig) {
                 assert!(orig < next);
-                self.curr[offset] = next;
+                self.curr.instructions[offset] = next;
             } else {
-                self.curr[offset] = I::first();
-                self.curr.push(I::first());
+                self.curr.instructions[offset] = I::first();
+                self.curr.instructions.push(I::first());
             }
             true
         } else {
@@ -33,45 +33,51 @@ impl<I: Instruction + std::cmp::PartialOrd> SearchAlgorithm for BruteForceSearch
 
     fn generate(&mut self) -> Option<Candidate<I>> {
         self.iterate(0);
-        if self.candidate().length() < self.max_length {
-            Some(self.candidate())
+        if self.curr.length() < self.max_length {
+            Some(self.curr.clone())
         } else {
             None
         }
+    }
+
+    fn peek(&self) -> &Candidate<I> {
+        &self.curr
     }
 }
 
 impl<I: Instruction + std::cmp::PartialOrd + std::cmp::PartialEq> BruteForceSearch<I> {
     /// Returns a new `BruteForceSearch<I>`
     pub fn new() -> Self {
-        Self { curr: vec![], max_length: usize::MAX }
+        Self {
+            curr: Candidate::empty(),
+            max_length: usize::MAX,
+        }
     }
 
     /// Returns a new `BruteForceSearch<I>`, which stops iterating when it hits a program of length
     /// `max_length`.
     pub fn new_with_length(max_length: usize) -> Self {
-        Self { curr: vec![], max_length }
+        Self {
+            curr: Candidate::empty(),
+            max_length,
+        }
     }
 
     fn iterate(&mut self, offset: usize) {
-        if offset >= self.curr.len() {
+        if offset >= self.curr.instructions.len() {
             // We've run off the current length of the vector, so append another instruction
-            self.curr.push(I::first());
+            self.curr.instructions.push(I::first());
             return;
         }
 
-        if let Some(insn) = self.curr[offset].increment() {
-            self.curr[offset] = insn;
+        if let Some(insn) = self.curr.instructions[offset].increment() {
+            self.curr.instructions[offset] = insn;
             return;
         }
 
         // We've exhausted all possibilities for this offset; try the next offset
-        self.curr[offset] = I::first();
+        self.curr.instructions[offset] = I::first();
         self.iterate(offset + 1);
-    }
-
-    fn candidate(&self) -> Candidate<I> {
-        Candidate::new(self.curr.clone())
     }
 }
 
