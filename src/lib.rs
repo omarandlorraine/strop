a6d277ff837b6a94ab2b9171395bd2c498944fe6
Sam M W <you@example.com>
2024-07-19 09:08:02 +0100

    public instructions member for Candidate<I>

--- src/lib.rs
+++ src/lib.rs
@@ -203,7 +203,7 @@ pub trait Emulator<T: Instruction> {
 /// A candidate program. This is essentially an ordered list of `Instruction`s.
 #[derive(Clone, Debug, Default, PartialEq)]
 pub struct Candidate<T: Instruction> {
-    instructions: Vec<T>,
+    pub instructions: Vec<T>,
 }
 
 impl<T: Instruction> Candidate<T> {
