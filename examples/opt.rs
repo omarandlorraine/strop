ffae457dd85284a27b630405c094adb0f45ccf33
Sam M W <you@example.com>
2024-08-20 18:59:29 +0100

    start an example

---
+++ examples/opt.rs
@@ -0,0 +1,15 @@
+//! An example of a program that uses strop to optimize an existing function.
+
+use strop::z80::Insn;
+use strop::z80::SdccCall1;
+
+fn main() {
+    // Construct the existing machine code.
+    let mc = vec![
+        Insn::new(&[0x06, 0x40]), // LD B,40H
+        Insn::new(&[0x0e, 0x7f]), // LD C,7FH
+        Insn::new(&[0xc9]),       // RET
+    ];
+
+    // This machine code is callable using the sdcccall(1) calling convention.
+}
