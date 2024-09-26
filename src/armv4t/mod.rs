8c5249adbf1841ed412f3ed3a697467a4f90c913
Sam M W <you@example.com>
2024-09-26 13:55:04 +0100

    add emu and function

--- src/armv4t/mod.rs
+++ src/armv4t/mod.rs
@@ -1,6 +1,10 @@
 //! The `armv4t` module, the strop back-end for targeting the ARMv4T CPUs, including the ARM7TDMI.
 mod diss;
+mod emu;
+mod function;
 pub mod isa;
 mod subroutine;
 
+pub use emu::Emulator;
 pub use isa::Insn;
+pub use subroutine::Subroutine;
