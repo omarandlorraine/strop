dbdee1fe38d36ed2a0ccde74f18f553e4208a888
Sam M W <you@example.com>
2024-11-05 09:15:37 +0000

    switch strop to `split` branch, upgrade mos6502

--- src/bruteforce.rs
+++ src/bruteforce.rs
@@ -39,42 +39,60 @@ impl<
         }
     }
 
-    /// Returns the next function that matches the target function
-    pub fn search(&mut self) -> Option<U> {
-        loop {
-            if !self.candidate.step() {
-                return None;
-            }
-            self.candidate.fixup();
+    /// Returns the candidate currently under consideration
+    pub fn candidate(&self) -> &U {
+        &self.candidate
+    }
 
-            match test::passes(&self.candidate, &self.tests) {
-                Err(StropError::DidntReturn) => {
-                    // The candidate does not pass the test case(s)
-                    // go round the loop again
-                }
-                Err(StropError::Undefined) => {
-                    // The candidate does not pass the test case(s)
-                    // go round the loop again
-                }
-                Ok(false) => {
-                    // The candidate does not pass the test case(s)
-                    // go round the loop again
-                }
-                Ok(true) => {
-                    // Found a candidate which passes all known test cases.
-                    // Let's fuzz test the candidate
-                    if let Some(test_case) =
-                        test::fuzz(&self.target_function, &self.candidate, 5000)
+    /// Advances the candidate to the next position in the search space
+    pub fn step(&mut self) -> bool {
+        if !self.candidate.step() {
+            return false;
+        }
+        self.candidate.fixup();
+        true
+    }
+
+    /// Tests that the candidate matches the target function
+    pub fn test(&mut self) -> bool {
+        match test::passes(&self.candidate, &self.tests) {
+            Err(StropError::DidntReturn) => {
+                // The candidate does not pass the test case(s)
+                false
+            }
+            Err(StropError::Undefined) => {
+                // The candidate does not pass the test case(s)
+                false
+            }
+            Ok(false) => {
+                // The candidate does not pass the test case(s)
+                false
+            }
+            Ok(true) => {
+                // Found a candidate which passes all known test cases.
+                // Let's fuzz test the candidate
+                if let Some(test_case) =
+                    test::fuzz(&self.target_function, &self.candidate, 5000)
                     {
                         // We've fuzzed the functions against eachother and found another test case.
                         // So keep hold of this new test case
                         self.tests.push(test_case);
+                        false
                     } else {
                         // The candidate passed all known test cases and also a fuzz test, so let's say
                         // it's good enough and return it
-                        return Some(self.candidate.clone());
+                        true
                     }
-                }
+            }
+        }
+    }
+
+    /// Returns the next function that matches the target function
+    pub fn search(&mut self) -> Option<U> {
+        loop {
+            self.step();
+            if self.test() {
+                return Some(self.candidate.clone());
             }
         }
     }
