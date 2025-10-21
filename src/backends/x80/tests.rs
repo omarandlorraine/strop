b53e64c9ce08ad6e591009663b31b7204aae7ba7
omarandlorraine <64254276+omarandlorraine@users.noreply.github.com>
2025-10-21 04:58:27 +0100

    x80

---
+++ src/backends/x80/tests.rs
@@ -0,0 +1,20 @@
+use crate::backends::x80::X80;
+use crate::backends::x80::data::InstructionData;
+
+// The instruction set includes loads of useless instructions like `ld b, b`, the loads the B
+// register with itself. Having no effect on flags or anything. These instructions are NOPs, but
+// not the canonical NOP.
+fn not_a_useless_move(data: &InstructionData) {
+    if data.mnemonic == "ld" {
+        assert_ne!(data.operands[0], data.operands[1], "{:?}", data);
+    }
+}
+
+pub(crate) fn std_x80_tests<I: X80>() {
+    let mut i = I::first();
+
+    while i.increment().is_ok() {
+        let data = i.decode();
+        not_a_useless_move(data);
+    }
+}
