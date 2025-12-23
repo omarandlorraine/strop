89b7ae04fc9be678a53544f887a33e8f1ab7e20b
omarandlorraine <64254276+omarandlorraine@users.noreply.github.com>
2025-12-23 16:21:07 +0000

    gen example now has switches for -K and -v

--- examples/gen.rs
+++ examples/gen.rs
@@ -16,6 +16,9 @@ fn main() {
     let mut args = std::env::args();
     let _ = args.next().unwrap();
 
+    let mut verbose = false;
+    let mut keep_going = false;
+
     // First figure out which target we're generating code for
     let Some(target) = args.next() else {
         println!("Specify the target.");
@@ -27,15 +30,28 @@ fn main() {
         help_list_triplets();
     };
 
+    while let Some(switch) = args.next() {
+        if switch == "-v" {
+            verbose = true;
+        } else if switch == "-K" {
+            keep_going = true;
+        } else {
+            println!("Unknown switch {switch:?}.");
+            std::process::exit(1);
+        }
+    }
+
     let mut searcher = target.pure_leaf_function_search(add5 as fn(u8) -> RunResult<u8>);
 
     loop {
         searcher.increment();
         if searcher.pass() {
             println!("solution:");
-            println!("{searcher}");
-            break;
-        } else {
+            println!("{searcher:?}");
+            if !keep_going {
+                break;
+            }
+        } else if verbose{
             println!("next_try:");
             println!("{searcher:?}");
         }
