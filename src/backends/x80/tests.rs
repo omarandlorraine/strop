e5c5035c3ff495f133fcd088dbd62295874525dd
omarandlorraine <64254276+omarandlorraine@users.noreply.github.com>
2026-03-23 15:07:31 +0000

    remove x80 tests

--- src/backends/x80/tests.rs
+++
@@ -1,9 +0,0 @@
-use crate::backends::x80::X80;
-
-// checks if the flow control bit is set correctly in the instruction data thing. basically it has
-// to agree with what the opcode says it is.
-fn flow_control<I: X80>(insn: &I) {
-}
-
-fn length<I: X80>(insn: &I) {
-}
